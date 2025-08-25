1f61de4ace1038106e3b50a4edf2ca5a6e37b02e
NEPotato <james.boydens@gmail.com>
2025-08-25 15:21:09 +0200

    global_form_style

--- src/components/AddArticle.vue
+++ src/components/AddArticle.vue
@@ -31,16 +31,53 @@ const handleSubmit = () => {
 </script>
 
 <template>
-  <section class="card">
+  <section class="add-article">
     <p>add article</p>
-    <form action="" @submit.prevent="handleSubmit">
-      <label for="title">Article's title<input required type="text" name="title" v-model="title" /></label>
-      <label for="abstract">Article's abstract<input required type="text" name="abstract" v-model="abstract" /></label>
-      <label for="content">Article's content<textarea required name="content" v-model="content" cols="30" rows="10"></textarea></label>
-      <input type="submit"> Submit
+    <form action="" @submit.prevent="handleSubmit" class="add-article__form">
+      <label class="add-article__form__label" for="title">
+        Article's title<input required type="text" name="title" v-model="title" />
+      </label>
+      <label class="add-article__form__label" for="abstract">
+        Article's abstract<input required type="text" name="abstract" v-model="abstract" />
+      </label>
+      <label class="add-article__form__label" for="content">
+        Article's content<textarea required name="content" v-model="content" cols="30" rows="10"></textarea>
+      </label>
+      <input type="submit" class="add-article__form__button">
     </form>
   </section>
 </template>
 
 <style scoped lang="scss">
+.add-article {
+  margin: 2rem 0;
+  border: 1px solid #ccc;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  &__form {
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+    &__label {
+      display: flex;
+      gap: .5rem;
+    }
+    &__button {
+      border: 1px solid #ccc;
+      padding-inline: 1.5rem;
+      padding-block: 0.5rem;
+      cursor: pointer;
+      background-color: #02b;
+      color: #ccc;
+      border-radius: 0.5rem;
+      transition: all 0.3s ease-in-out;
+      &:hover {
+        background-color: #ccc;
+        color: #02b;
+      }
+    }
+  }
+}
 </style>
